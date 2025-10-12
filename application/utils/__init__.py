d0d686944aaa168b399744ab3b60960e50a5f5f5
k00gar <arnaud.chevreux@gmail.com>
2025-10-12 11:51:30 +0200

    Simplify keyboard shortcuts by removing profiles feature
    Remove the entire shortcut profiles system as it was overcomplicated
    for the application's needs. Users can now directly edit shortcuts
    without managing multiple profiles.
    
    Changes:
    - Remove Profiles tab from keyboard shortcuts dialog
    - Remove ShortcutProfileManager and related code
    - Delete shortcut_profiles.py (278 lines)
    - Simplify keyboard_shortcuts_dialog.py (357 lines reduced)
    - Move conflict detection directly into dialog class
    - Remove profile imports from utils package
    
    The keyboard shortcuts dialog is now simpler with just two tabs:
    Shortcuts and Settings.

--- application/utils/__init__.py
+++ application/utils/__init__.py
@@ -15,4 +15,3 @@ from .video_segment import VideoSegment
 from .write_access import check_write_access
 from .button_styles import primary_button_style, destructive_button_style
 from .keyboard_layout_detector import KeyboardLayoutDetector, KeyboardLayout, get_layout_detector
-from .shortcut_profiles import ShortcutProfile, ShortcutProfileManager
